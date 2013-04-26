b9dd71d37c5a58c45ed7be590ec2484ecbec54a0
Wictor Wilén <wictorwilen@users.noreply.github.com>
2013-04-26 14:02:44 +0000

    Added SharePoint client components

---
+++ Main/OpenWOPI.SharePoint/OpenWOPI.SharePoint/Features/FarmOpenWOPI/FarmOpenWOPI.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="66bca163-518f-4f9c-8ef0-dbfd374b444f" featureId="66bca163-518f-4f9c-8ef0-dbfd374b444f" imageUrl="" scope="Farm" solutionId="00000000-0000-0000-0000-000000000000" title="OpenWOPI Farm Feature" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="2572cfb6-10b1-4734-a0b3-6c755ca07674" />
+  </projectItems>
+</feature>
---
+++ Main/OpenWOPI.SharePoint/OpenWOPI.SharePoint/Layouts/OpenWOPI.SharePoint/previews.js
@@ -0,0 +1,12 @@
+﻿function OpenWOPIPreviews() {
+    SP.SOD.executeOrDelayUntilScriptLoaded(function () {
+        filePreviewManager.previewers.extensionToPreviewerMap.txt = [embeddedWACPreview, WACImagePreview]
+        filePreviewManager.previewers.extensionToPreviewerMap.gpx = [embeddedWACPreview, WACImagePreview]
+        filePreviewManager.previewers.extensionToPreviewerMap.vsdx = [embeddedWACPreview, WACImagePreview]
+        embeddedWACPreview.dimensions.txt = { width: 400, height: 400 }
+        embeddedWACPreview.dimensions.gpx = { width: 400, height: 400 }
+        embeddedWACPreview.dimensions.vsdx = { width: 200, height: 200 }
+    }, "filepreview.js");
+    notifyScriptsLoadedAndExecuteWaitingJobs("OpenWOPI.SharePoint/previews.js");
+}
+OpenWOPIPreviews();
