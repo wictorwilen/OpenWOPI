bfe712c09f014607a4c1495e04d4649f9cd5caa5
Wictor Wilén <wictorwilen@users.noreply.github.com>
2013-04-26 14:05:08 +0000

    Added TextApp

---
+++ Main/OpenWOPI/OpenWOPI.Client.Web/Models/TextAppModel.cs
@@ -0,0 +1,21 @@
+﻿using OpenWOPI.Client.Mvc;
+using OpenWOPI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenWOPI.Client.Web.Models
+{
+    public class TextAppModel : OpenWOPIModel
+    {
+        public override OpenWOPIDocument ViewDocument(string source, OpenWOPIAccessToken accessToken, bool loadMetadata = false)
+        {
+            OpenWOPITextDocument doc = new OpenWOPITextDocument(source, accessToken, OpenWOPIProofKey.ReadFromConfiguration(OpenWOPIClientConfiguration.Current));
+            if (loadMetadata)
+                doc.CheckFileInfo();
+            doc.GetFile();
+            return doc;
+        }
+    }
+}
