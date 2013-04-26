5012095d18082a4f8c8db8a2665545753fe0756b
wictor_cp <SND\wictor_cp>
2013-04-26 14:02:44 +0000

    Added SharePoint client components

---
+++ Main/OpenWOPI.SharePoint/OpenWOPI.SharePoint/OpenWOPIJavaScriptRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.SharePoint.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace OpenWOPI.SharePoint
+{
+    [MdsCompliant(true)]
+    public class OpenWOPIJavaScriptRegistration: WebControl
+    {
+        protected override void OnPreRender(EventArgs e)
+        {
+            ScriptLink.RegisterScriptAfterUI(this.Page, "OpenWOPI.SharePoint/previews.js", false);
+
+        }
+
+    }
+}
